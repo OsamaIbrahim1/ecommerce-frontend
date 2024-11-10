4113487789b88d29648fbc2ad0416aadf7735013
OsamaIbrahim1 <oi5921435@gmial.com>
2024-11-10 18:39:29 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./App.{js,jsx,ts,tsx}",
+  "./src/**/*.{js,jsx,ts,tsx}",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
